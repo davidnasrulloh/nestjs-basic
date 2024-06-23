679a4dba634f2881bd0431753ce30a1e29a8ae7f
David Nasrulloh <davidkrb52@gmail.com>
2024-06-23 18:08:56 +0700

    fix: add manual http

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -1,31 +1,26 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
-import { PrismaService } from "./prisma.service";
-import { Request } from "express";
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-
-    constructor(private prismaServie: PrismaService){
-
-    }
+    constructor(private prismaServie: PrismaService) {}
 
     async use(req: any, res: any, next: (error?: any) => void) {
-        const token:any = req.headers['authorization'] as string;
-        
-        if(token){
+        const token: any = req.headers['authorization'] as string;
+
+        if (token) {
             const user = await this.prismaServie.user.findFirst({
                 where: {
-                    token: token //.split(' ')[1]
-                }
-            })
+                    token: token, //.split(' ')[1]
+                },
+            });
             // console.log('itemku user :', token.split(' ')[1], user)
 
-            if(user){
-                req.user = user
+            if (user) {
+                req.user = user;
             }
         }
 
-        next()
+        next();
     }
-    
 }
